6c66c79a04bab3d2cbea3ebc7d5871febb526307
Odinn <alejandro_xp90@hotmail.com>
2020-06-16 20:22:54 -0500

    selenium intermedio

--- src/basico/Ejercicio001TestNG.java
+++ src/basico/Ejercicio001TestNG.java
@@ -1,18 +1,16 @@
 package basico;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 public class Ejercicio001TestNG {
 
     private WebDriver driver;
-
-    private String userName = "";
-    private String password = "";
     public String xChromePath = System.getProperty("user.dir") + "\\drivers\\chromedriver.exe";
 
     /* Metodo para navegar a la pagina antes de correr el test*/
@@ -22,15 +20,25 @@ public class Ejercicio001TestNG {
         System.setProperty("webdriver.chrome.driver", xChromePath);
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.flipkart.com");
+        driver.get("https://google.com");
         driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
 
     }
 
     @Test
-    public void loginY(){
-        System.out.println("Inicio del Test 01");
+    @Parameters({"author","searchKey"})
+    public void testParameterWithXML(@Optional("Abc") String author, String searchKey) throws InterruptedException{
 
+        WebElement searchText = driver.findElement(By.name("q"));
+        //Searching text in google text box
+        searchText.sendKeys(searchKey);
+
+        System.out.println("Welcome ->"+author+" Your search key is->"+searchKey);
+        System.out.println("Thread will sleep now");
+        Thread.sleep(3000);
+        System.out.println("Value in Google Search Box = "+searchText.getAttribute("value") +" ::: Value given by input = "+searchKey);
+        //verifying the value in google search box
+        Assert.assertTrue(searchText.getAttribute("value").equalsIgnoreCase(searchKey));
 
     }
 
